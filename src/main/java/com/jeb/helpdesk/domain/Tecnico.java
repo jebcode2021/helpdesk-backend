b688adc4bc956a591b724f0a9981b91296de4614
jebcode2021 <jebcode@outlook.com.br>
2023-01-23 14:00:31 -0300

    endpoint create para Tecnico

--- src/main/java/com/jeb/helpdesk/domain/Tecnico.java
+++ src/main/java/com/jeb/helpdesk/domain/Tecnico.java
@@ -2,8 +2,10 @@ package com.jeb.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.jeb.helpdesk.domain.dtos.TecnicoDTO;
 import com.jeb.helpdesk.domain.enums.Perfil;
 
 import jakarta.persistence.Entity;
@@ -26,7 +28,18 @@ public class Tecnico extends Pessoa{
 		super(id, nome, cpf, email, senha);
 		addPerfils(Perfil.CLIENTE);
 	}
-
+	
+	public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfils = obj.getPerfils().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+	}
+	
 	public List<Chamado> getChamados() {
 		return chamados;
 	}
